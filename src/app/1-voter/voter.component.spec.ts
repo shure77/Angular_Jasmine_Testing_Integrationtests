5d8b3ddab0e8d0eb6260ece59ada8f49d5f96447
Thaler <Roman.Thaler@adesso.at>
2019-08-19 15:04:31 +0200

    Integration test - check for innerHtml and class

---
+++ src/app/1-voter/voter.component.spec.ts
@@ -0,0 +1,39 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { VoterComponent } from './voter.component';
+
+describe('VoterComponent', () => {
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
+
+  // create a dynamic testing module in beforeEach() - This is the basic setup for writing integration tests
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VoterComponent ]
+    });
+
+    // create an instance of the component
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
+    // fixture.nativeElement get the root DOM element of this component
+    // fixture.debugElement is a wrapping element around the native Element
+  });
+
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges(); //has do be done to update the DOM and run Angulars change detection
+
+    let de = fixture.debugElement.query(By.css('.vote-count'));
+    let el: HTMLElement = de.nativeElement;
+    expect(el.innerText).toContain('21');
+  });
+
+  it('should highlight the upvote button if I have upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges(); //has do be done to update the DOM and run Angulars change detection
+
+    let de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+    expect(de.classes['highlighted']).toBeTruthy();
+  });
+});
